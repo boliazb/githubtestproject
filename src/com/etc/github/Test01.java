28e327f069799882c8f6c10147153d064dffdaf2
boliazb <836856003@qq.com>
2020-08-06 22:13:32 +0800

    Initial commit

---
+++ src/com/etc/github/Test01.java
@@ -0,0 +1,12 @@
+package com.etc.github;
+
+/**
+ * @author ZHOUB
+ * @create 2020-08-06-22:10
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println( "hellloword");
+    }
+}
